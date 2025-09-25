92df6847918b03b8a908df4fe52053b6cf53eaa8
3andman <dbjsam@gmail.com>
2025-09-25 14:22:26 -0400

    Added starry background
    realized searching up your problems is amazingly useful

--- Art-Jam/js/script.js
+++ Art-Jam/js/script.js
@@ -1,24 +1,35 @@
 /**
  * Title of Project
- * Author Name
- * 
+ * Dylan Samaan
+ *
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
 
 "use strict";
+let stars = [];
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
 function setup() {
+  createCanvas(1000, 800);
+  noStroke();
 
+  // Making Stars
+  for (let i = 0; i < 300; i++) {
+    stars.push({
+      x: random(width),
+      y: random(height),
+      size: random(1, 3),
+    });
+  }
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+  background(10, 10, 30);
 
+  for (let i = 0; i < stars.length; i++) {
+    let star = stars[i];
+    let brightness = map(sin(frameCount * 0.02 + star.phase), -1, 1, 150, 255);
+    fill(brightness);
+    ellipse(star.x, star.y, star.size);
+  }
 }
