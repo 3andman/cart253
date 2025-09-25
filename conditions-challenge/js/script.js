318138d3a7075c8806b75d6dffa9272b6bf080c7
3andman <dbjsam@gmail.com>
2025-09-24 20:37:34 -0400

    donezo

--- conditions-challenge/js/script.js
+++ conditions-challenge/js/script.js
@@ -1,50 +1,144 @@
 /**
- * Title of Project
- * Author Name
+ * Puck Off
+ * Dylan Samaan
  *
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Pushin pucks into targets baby
  */
 
 "use strict";
 
-let spider = {
+const puck = {
   x: 500,
   y: 500,
-  size: 50,
-  speedX: 5,
-  speedY: 6,
+  size: 100,
+  fill: "#8e536dff",
+  fills: {
+    noOverlap: "#8e536dff",
+    overlap: "#ffffff",
+  },
 };
 
+const user = {
+  x: undefined, // will be mouseX
+  y: undefined, // will be mouseY
+  size: 75,
+  fill: "#ffffffff",
+  fills: {
+    noOverlap: "#ffffff",
+    overlap: "#8e536dff",
+  },
+};
+
+const target = {
+  x: 250,
+  y: 250,
+  size: 200,
+  fill: "#391e33ff",
+  fills: {
+    noOverlap: "#391e33ff",
+    overlap: "#ffffffff",
+  },
+};
+
+/**
+ * Create the canvas
+ */
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(1000, 1000);
 }
 
-function moveCircle() {
-  spider.x += spider.speedX;
-  spider.y += spider.speedY;
+function movePuck() {}
 
-  if (spider.y > height) {
-    //spider.y = 0;
-    spider.speedY = spider.speedY * -1;
+/**
+ * Move the user circle, check for overlap, draw the two circles
+ */
+function draw() {
+  background("#e5c0daff");
+
+  // Move user circle
+  moveUser();
+
+  // Draw the user and puck
+  drawUser();
+  drawPuck();
+
+  // Draw the Target
+  drawTarget();
+
+  // Move the puck
+  movePuck();
+
+  let dx = puck.x - user.x;
+  let dy = puck.y - user.y;
+
+  const d = dist(puck.x, puck.y, user.x, user.y);
+
+  const overlap = d < puck.size / 2 + user.size / 2;
+
+  const dt = dist(target.x, target.y, puck.x, puck.y);
+
+  const toverlap = dt < puck.size / 2 + target.size / 2;
+
+  if (overlap) {
+    user.fill = user.fills.overlap;
+    puck.fill = puck.fills.overlap;
+    if (dx > 0) {
+      puck.x += dx / 20;
+    } else if (dx < 0) {
+      puck.x += dx / 20;
+    }
+    if (dy > 0) {
+      puck.y += dy / 20;
+    } else if (dy < 0) {
+      puck.y += dy / 20;
+    }
+  } else {
+    user.fill = user.fills.noOverlap;
+    puck.fill = puck.fills.noOverlap;
   }
 
-  if (spider.x > width) {
-    //spider.x = 0;
-    spider.speedX = spider.speedX * -1;
+  if (toverlap) target.fill = target.fills.overlap;
+  else {
+    target.fill = target.fills.noOverlap;
   }
 }
 
-function drawCircle() {
+/**
+ * Sets the user position to the mouse position
+ */
+function moveUser() {
+  user.x = mouseX;
+  user.y = mouseY;
+}
+
+/**
+ * Displays the user circle
+ */
+function drawUser() {
   push();
-  fill("#ffff");
   noStroke();
-  ellipse(spider.x, spider.y, spider.size, spider.size);
+  fill(user.fill);
+  ellipse(user.x, user.y, user.size);
   pop();
 }
 
-function draw() {
-  background("#c49fc2ff");
-  drawCircle();
-  moveCircle();
+/**
+ * Displays the puck circle
+ */
+function drawPuck() {
+  push();
+  noStroke();
+  fill(puck.fill);
+  ellipse(puck.x, puck.y, puck.size);
+  pop();
+}
+/**
+ * Displays the target
+ */
+function drawTarget() {
+  push();
+  noStroke();
+  fill(target.fill);
+  ellipse(target.x, target.y, target.size);
+  pop();
 }
