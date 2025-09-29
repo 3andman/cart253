fcc5de1a5710d6f9b7eae8137ae24ccf54786639
3andman <dbjsam@gmail.com>
2025-09-28 21:33:21 -0400

    addes a meteor shower on mouse click as well as tidied up

--- Art-Jam/js/script.js
+++ Art-Jam/js/script.js
@@ -31,29 +31,7 @@ function setup() {
 function draw() {
   background(10, 10, 30);
 
-  // Adds Shooting Stars
-
-  // Only spawn when enough frames have passed since last one
-  if (frameCount >= nextShootingStar) {
-    let startX = -50;
-    let startY = random(height / 2);
-
-    // Always downward-right
-    let angle = random(PI / 6, PI / 3);
-    let speed = random(4, 7);
-
-    shootingStars.push({
-      x: startX,
-      y: startY,
-      vx: cos(angle) * speed,
-      vy: sin(angle) * speed * 0.5,
-      len: random(3, 6),
-    });
-
-    // Force the next star to wait 500–900 frames (≈8–15 seconds)
-    nextShootingStar = frameCount + int(random(400, 800));
-  }
-  // Draw the Twinkling Stars
+  //  Twinkling Stars
   for (let i = 0; i < stars.length; i++) {
     let star = stars[i];
     let brightness = map(
@@ -66,12 +44,31 @@ function draw() {
     fill(brightness);
     ellipse(star.x, star.y, star.size);
   }
-  // Update Shooting Stars
+
+  //  Spawn Shooting Stars
+  if (frameCount >= nextShootingStar) {
+    let startX = -50;
+    let startY = random(height / 2);
+
+    let angle = random(PI / 6, PI / 3);
+    let speed = random(4, 7);
+
+    shootingStars.push({
+      x: startX,
+      y: startY,
+      vx: cos(angle) * speed,
+      vy: sin(angle) * speed * 0.5,
+      len: random(1, 3),
+    });
+
+    // Wait before next one
+    nextShootingStar = frameCount + int(random(300, 500));
+  }
+
+  //  Draw & Update Shooting Stars
   for (let i = shootingStars.length - 1; i >= 0; i--) {
     let s = shootingStars[i];
 
-    // head is (s.x, s.y)
-    // tail is behind along velocity
     let tailX = s.x - s.vx * s.len;
     let tailY = s.y - s.vy * s.len;
 
@@ -83,14 +80,30 @@ function draw() {
     fill(255);
     ellipse(s.x, s.y, 3, 3);
 
-    // update position
     s.x += s.vx;
     s.y += s.vy;
 
-    // remove if off-screen
     if (s.x < -200 || s.x > width + 200 || s.y > height + 200) {
       shootingStars.splice(i, 1);
     }
   }
-  noStroke();
+}
+// Spawn Meteor Shower
+function mousePressed() {
+  let count = int(random(4, 7));
+  for (let i = 0; i < count; i++) {
+    let startX = -50;
+    let startY = random(height / 2);
+
+    let angle = random(PI / 6, PI / 3);
+    let speed = random(6, 9);
+
+    shootingStars.push({
+      x: startX,
+      y: startY,
+      vx: cos(angle) * speed,
+      vy: sin(angle) * speed * 0.5,
+      len: random(1, 3),
+    });
+  }
 }
