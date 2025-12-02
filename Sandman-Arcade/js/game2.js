8ab6f64c272991a7d4af7df5ef7d32515cf4268b
3andman <dbjsam@gmail.com>
2025-12-01 19:03:43 -0500

    starting game 2
    gonna be like flappy bird

--- Sandman-Arcade/js/game2.js
+++ Sandman-Arcade/js/game2.js
@@ -8,12 +8,36 @@
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
- */
-function setup() {}
+//Assets
+let bgImg;
+let birdImg;
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
- */
-function draw() {}
+//Game State
+let gameStart = true;
+let gameOver = false;
+let score = 0;
+
+//Background Scrolling
+let bgx = 0;
+let bgSpeed = 3;
+
+//Bird
+const bird = {
+  x: 0,
+  y: 0,
+  vy: 0,
+  size: 80,
+};
+//Physics
+const gravity = 0.6;
+constjumpStrenght = -10;
+
+//Load Assets
+function preload() {
+  bgImg = loadImage("assets/game2/bg-loop.png");
+  birdImg = loadImgae("assets/game2/bird.png");
+}
+
+function setup() {
+  const aspect = 1280 / 1080;
+}
